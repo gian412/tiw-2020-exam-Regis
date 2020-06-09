799432f92b8feff25c37125643a131f094ec0b1d
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-06-09 23:57:56 +0200

    Implemented bean for Account

---
+++ src/it/polimi/tiw/bank/beans/Account.java
@@ -0,0 +1,33 @@
+package it.polimi.tiw.bank.beans;
+
+public class Account {
+
+    private int id;
+    private long balance;
+    private int owner;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    public int getOwner() {
+        return owner;
+    }
+
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
+
+}
