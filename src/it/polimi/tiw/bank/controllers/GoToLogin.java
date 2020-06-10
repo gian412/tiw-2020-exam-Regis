f2cf1ace228e6058c4b4f2e19ed7667ba5234960
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-06-10 03:14:37 +0200

    Implemented basic servlet that redirect to login page

---
+++ src/it/polimi/tiw/bank/controllers/GoToLogin.java
@@ -0,0 +1,57 @@
+package it.polimi.tiw.bank.controllers;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Login")
+public class GoToLogin extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private TemplateEngine templateEngine;
+
+    public GoToLogin() {
+        super();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver( servletContext );
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Redirect to login.html
+        ServletContext servletContext = getServletContext();
+        final WebContext ctx = new WebContext(req, resp, servletContext, req.getLocale());
+        String path = "/login.html";
+        templateEngine.process(path, ctx, resp.getWriter());
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+}
