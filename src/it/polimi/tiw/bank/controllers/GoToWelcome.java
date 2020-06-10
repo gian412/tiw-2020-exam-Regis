7fe718b06627bcd8283a24326a0c3f5ec22e2472
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-06-10 03:18:43 +0200

    Implemented servlet that redirect to welcome.html

---
+++ src/it/polimi/tiw/bank/controllers/GoToWelcome.java
@@ -0,0 +1,62 @@
+package it.polimi.tiw.bank.controllers;
+
+import it.polimi.tiw.bank.utils.MultiPathMessageResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Welcome")
+public class GoToWelcome extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private TemplateEngine templateEngine;
+
+    public GoToWelcome() {
+        super();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver( servletContext );
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        this.templateEngine.setMessageResolver(new MultiPathMessageResolver(servletContext, "i18n"));
+        templateResolver.setSuffix(".html");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // Redirect to welcome.html
+        ServletContext servletContext = getServletContext();
+        final WebContext ctx = new WebContext(req, resp, servletContext, req.getLocale());
+        String path = "/welcome.html";
+        templateEngine.process(path, ctx, resp.getWriter());
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+}
