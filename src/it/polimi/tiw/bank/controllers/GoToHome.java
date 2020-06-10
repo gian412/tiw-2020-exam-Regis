699d5d206880f6897a235c29400c53e800eaf51a
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-06-10 03:16:49 +0200

    Added attribute in GoToHome class

--- src/it/polimi/tiw/bank/controllers/GoToHome.java
+++ src/it/polimi/tiw/bank/controllers/GoToHome.java
@@ -1,11 +1,16 @@
 package it.polimi.tiw.bank.controllers;
 
+import org.thymeleaf.TemplateEngine;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import java.sql.Connection;
 
 @WebServlet("/Home")
 public class GoToHome extends HttpServlet {
 
-
+    private static final long serialVersionUID = 1L;
+    private Connection connection;
+    private TemplateEngine templateEngine;
 
 }
