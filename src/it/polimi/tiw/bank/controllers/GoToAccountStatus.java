21cae047ba416f04ad2592294fdf1c98403e5d65
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-06-11 04:40:49 +0200

    Initialized /AccountStatus servlet

---
+++ src/it/polimi/tiw/bank/controllers/GoToAccountStatus.java
@@ -0,0 +1,78 @@
+package it.polimi.tiw.bank.controllers;
+
+import it.polimi.tiw.bank.utils.ClientHandler;
+import it.polimi.tiw.bank.utils.MultiPathMessageResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.xpath.XPath;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/AccountStatus")
+public class GoToAccountStatus extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private Connection connection;
+    private TemplateEngine templateEngine;
+
+    public GoToAccountStatus() {
+        super();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        connection = ClientHandler.getConnection(getServletContext());
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver( servletContext );
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        this.templateEngine.setMessageResolver(new MultiPathMessageResolver(servletContext, "i18n"));
+        templateResolver.setSuffix(".html");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String id;
+        int accountId;
+        try {
+            id = req.getParameter("id");
+        } catch (NullPointerException e) {
+            e.printStackTrace(); // TODO: remove after test
+            ServletContext servletContext = getServletContext();
+            final WebContext ctx = new WebContext(req, resp, servletContext, req.getLocale());
+            ctx.setVariable("errorMessage", "Unable to retrieve account status from DB");
+            String path = "/accountStatus.html";
+            templateEngine.process(path, ctx, resp.getWriter());
+            return;
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+
+        try {
+            ClientHandler.closeConnection(connection);
+        } catch (SQLException e) {
+            e.printStackTrace(); // TODO: remove after test
+        }
+
+    }
+}
