269df123eacfdb1d278c5163e2952984204e4555
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-06-11 05:21:05 +0200

    Initialized AccountDAO

---
+++ src/it/polimi/tiw/bank/dao/AccountDAO.java
@@ -0,0 +1,69 @@
+package it.polimi.tiw.bank.dao;
+
+import it.polimi.tiw.bank.beans.Transfer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountDAO {
+
+    private final Connection connection;
+    private final int id;
+
+    public AccountDAO(Connection connection, int id) {
+        this.connection = connection;
+        this.id = id;
+    }
+
+    public List<Transfer> findOutgoingTransfer () throws SQLException {
+
+        String query = "SELECT * FROM transfer WHERE origin = ? ORDER BY date DESC";
+        List<Transfer> transfers = new ArrayList<>();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, this.id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    Transfer transfer = new Transfer();
+                    transfer.setId(resultSet.getInt("id"));
+                    transfer.setDate(resultSet.getDate("date"));
+                    transfer.setAmount(resultSet.getLong("amount"));
+                    transfer.setOriginAccount(resultSet.getInt("origin"));
+                    transfer.setDestinationAccount(resultSet.getInt("destination"));
+                    transfer.setCausal(resultSet.getString("causal"));
+                    transfers.add(transfer);
+                }
+            }
+        }
+        return transfers;
+
+    }
+
+    public List<Transfer> findIncomingTransfer () throws SQLException {
+
+        String query = "SELECT * FROM transfer WHERE destination = ? ORDER BY date DESC";
+        List<Transfer> transfers = new ArrayList<>();
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, this.id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    Transfer transfer = new Transfer();
+                    transfer.setId(resultSet.getInt("id"));
+                    transfer.setDate(resultSet.getDate("date"));
+                    transfer.setAmount(resultSet.getLong("amount"));
+                    transfer.setOriginAccount(resultSet.getInt("origin"));
+                    transfer.setDestinationAccount(resultSet.getInt("destination"));
+                    transfer.setCausal(resultSet.getString("causal"));
+                    transfers.add(transfer);
+                }
+            }
+        }
+        return transfers;
+
+    }
+}
