4844da3ad4f70c26ce3e87d4b8bd7ce55ac36e27
Gianluca Regis <gianluca.regis@mail.polimi.it>
2020-06-12 01:33:07 +0200

    Implemented createTransfer in TransferDAO

--- src/it/polimi/tiw/bank/dao/TransferDAO.java
+++ src/it/polimi/tiw/bank/dao/TransferDAO.java
@@ -3,16 +3,29 @@ package it.polimi.tiw.bank.dao;
 import it.polimi.tiw.bank.beans.User;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class TransferDAO {
 
     private final Connection connection;
-    private final int id;
 
     public TransferDAO(Connection connection, int id) {
         this.connection = connection;
-        this.id = id;
+    }
+
+    public void createTransfer(long amount, int originId, int destinationId, String causal) throws SQLException {
+
+        String query = "INSERT INTO transfer (amount, origin, destination, causal) VALUES(?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setLong(1, amount);
+            preparedStatement.setInt(2, originId);
+            preparedStatement.setInt(3, destinationId);
+            preparedStatement.setString(4, causal);
+            preparedStatement.executeUpdate();
+        }
+
     }
 
 }
